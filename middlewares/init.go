83f5993ba1cd829b59ce764c215b44f52324fac5
Tom <husky._lby@petalmail.com>
2023-04-27 08:27:50 +0800

    学习中

--- middlewares/init.go
+++ middlewares/init.go
@@ -13,15 +13,21 @@ func Init(ctx *gin.Context) {
 	fmt.Println(time.Now())      // 打印当前时间
 	fmt.Println(ctx.Request.URL) // 打印当前地址
 	req_header := ctx.Request.Header
-	// fmt.Println(req_header)
-	// for k, v := range req_header {
-	// 	fmt.Printf("%v : %v\n", k, v)
-	// }
 	if req_header["Token"] != nil {
 		fmt.Println("Token:", req_header["Token"])
 	} else {
 		fmt.Println("Token不存在")
 	}
+	// 因为主程序是永远不会退出的，所以说不用担心子协程会被提前退出
+	cCp := ctx.Copy()
+	go func() {
+		time.Sleep(5 * time.Second)
+		fmt.Println("我是子协程")
+		fmt.Println(cCp.Request.URL)
+		fmt.Println(cCp.Request.Header)
+		fmt.Println(cCp.Get("username"))
+		fmt.Println("子协程执行结束")
+	}()
 	ctx.Next() // 执行下一个中间件
 	endTime := time.Now()
 	fmt.Printf("程序用时：%v", endTime.Sub(startTime))
